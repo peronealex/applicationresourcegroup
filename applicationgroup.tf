d3903adf7ed6b9fc4fb24d36e892d7ab88103f4c
Alessio Perone <alessio.perone@lexisnexisrisk.com>
2020-12-23 09:56:39 +0000

    testing group

--- applicationgroup.tf
+++ applicationgroup.tf
@@ -1,5 +1,19 @@
-resource "azurerm_application_security_group" "first" {
+/*resource "azurerm_application_security_group" "first" {
   name                = "xpc-test1"
   location            = "westeurope"
   resource_group_name = var.resource_group_name
+}
+
+
+resource "azuread_group" "example" {
+  name    = "xpc-test1"
+  members = [
+    PeroneA@b2b.regn.net
+    /* more users */
+ // ]
+//} 
+
+
+resource "azuread_group" "example" {
+  name = "xpc-alessio"
 }
